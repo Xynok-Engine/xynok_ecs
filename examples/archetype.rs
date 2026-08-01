f58820f34322b614bfd7b7f9cc77a49dad48794b
monok-robeto <monokrobeto@gmail.com>
2026-08-01 15:56:22 +0700

    add tuple arch

--- examples/archetype.rs
+++ examples/archetype.rs
@@ -11,9 +11,31 @@ struct Mana(usize);
 #[derive(Default)]
 struct MoveSpeed(usize);
 
+fn new_hero() -> (Hp, Mana, MoveSpeed)
+{
+    (Hp::default(), Mana::default(), MoveSpeed::default())
+}
 fn main()
 {
     let mut world = World::default();
-    //world.create((Hp::default(), Mana::default(), MoveSpeed::default()));
-    println!("Hello ");
+
+    let a = world.create(new_hero());
+    let b = world.create(new_hero());
+    let c = world.create(new_hero());
+
+    println!("a: {}. exist() = {}", a, world.exists(a));
+    println!("b: {}. exist() = {}", b, world.exists(b));
+    println!("c: {}. exist() = {}", c, world.exists(c));
+
+    println!("\n---------------- destroy a");
+    world.destroy(a);
+    println!("a: {}. exist() = {}", a, world.exists(a));
+    println!("b: {}. exist() = {}", b, world.exists(b));
+    println!("c: {}. exist() = {}", c, world.exists(c));
+
+    let a = world.create((Hp::default(), Mana::default(), MoveSpeed::default()));
+    println!("\n---------------- create new entity for a, expect a.ver += 1");
+    println!("a: {}. exist() = {}", a, world.exists(a));
+    println!("b: {}. exist() = {}", b, world.exists(b));
+    println!("c: {}. exist() = {}", c, world.exists(c));
 }
--- src/archetype/variant.rs
+++ src/archetype/variant.rs
@@ -5,12 +5,12 @@ use crate::{
     },
     chunk::layout::ChunkLayout,
 };
-
+use std::any::TypeId;
 impl<T: TComponent + 'static> TArchetype for T
 {
     const COMPONENT_DESCRIPTORS: &[crate::apis::ComponentDescriptor] = &[T::COMPONENT_DESCRIPTOR];
-    const QUERY_TYPE_IDS: &[std::any::TypeId] = &[std::any::TypeId::of::<T::QueryType>()];
-    const STORAGE_TYPE_IDS: &[std::any::TypeId] = &[std::any::TypeId::of::<T::StorageType>()];
+    const QUERY_TYPE_IDS: &[std::any::TypeId] = &[TypeId::of::<T::QueryType>()];
+    const STORAGE_TYPE_IDS: &[std::any::TypeId] = &[TypeId::of::<T::StorageType>()];
 
     fn write_at(layout: &ChunkLayout, chunk: &mut crate::chunk::Chunk, write_idx: usize, val: Self) -> Result<(), XynokEcsError>
     {
@@ -22,3 +22,49 @@ impl<T: TComponent + 'static> TArchetype for T
         unsafe { chunk.take_at::<T>(layout, idx) }
     }
 }
+
+macro_rules! tuple_arch {
+    (
+        $([$component:ident,$idx:tt]),* $(,)?
+    ) =>
+    {
+        impl<$($component,)*> TArchetype for ($($component,)*)
+        where $($component: TComponent +'static,)*
+        {
+            const COMPONENT_DESCRIPTORS: &[crate::apis::ComponentDescriptor] = &[$($component::COMPONENT_DESCRIPTOR,)*];
+            const QUERY_TYPE_IDS: &[std::any::TypeId] = &[$(TypeId::of::<$component::QueryType>(),)*];
+            const STORAGE_TYPE_IDS: &[std::any::TypeId] = &[$(TypeId::of::<$component::StorageType>(),)*];
+
+            fn write_at(layout: &ChunkLayout, chunk: &mut crate::chunk::Chunk, write_idx: usize, val: Self) -> Result<(), XynokEcsError>
+            {
+                unsafe
+                {
+                    $(chunk.write_at::<$component>(layout, write_idx, val.$idx)?;)*
+                }
+                Ok(())
+            }
+
+            fn take_from(layout: &ChunkLayout, chunk: &mut crate::chunk::Chunk, idx: usize) -> Result<Self, XynokEcsError>
+            {
+                unsafe { Ok(($(chunk.take_at::<$component>(layout, idx)?,)*)) }
+            }
+
+        }
+    };
+}
+#[rustfmt::skip] tuple_arch!([C0, 0]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6], [C7, 7]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6], [C7, 7], [C8, 8]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6], [C7, 7], [C8, 8], [C9, 9]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6], [C7, 7], [C8, 8], [C9, 9], [C10, 10]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6], [C7, 7], [C8, 8], [C9, 9], [C10, 10], [C11, 11]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6], [C7, 7], [C8, 8], [C9, 9], [C10, 10], [C11, 11], [C12, 12]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6], [C7, 7], [C8, 8], [C9, 9], [C10, 10], [C11, 11], [C12, 12], [C13, 13]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6], [C7, 7], [C8, 8], [C9, 9], [C10, 10], [C11, 11], [C12, 12], [C13, 13], [C14, 14]);
+#[rustfmt::skip] tuple_arch!([C0, 0], [C1, 1], [C2, 2], [C3, 3], [C4, 4], [C5, 5], [C6, 6], [C7, 7], [C8, 8], [C9, 9], [C10, 10], [C11, 11], [C12, 12], [C13, 13], [C14, 14], [C15, 15]);
